9f1ad839795b6e5e02cc44cb1b9c0f5bf0673624
adelgado <adelgado0723@gmail.com>
2024-10-27 23:10:43 -0400

    adds -y flag to initial setup script

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 sudo apt install ansible
 git clone https://github.com/adelgado0723/smsolutions_home.git ~/belleview-microgreens/smsolutions_home
